fb2ebd4b0cd7116d7cb0e511069d810fa3043174
y.nakamura <y.nakamura@linecorp.com>
2023-12-12 08:43:24 +0900

    Add SampleUtils

---
+++ src/test/java/com/example/checkstylereviewdogsample/utils/SampleUtilsTest.java
@@ -0,0 +1,10 @@
+package com.example.checkstylereviewdogsample.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleUtilsTest {
+    @Test
+    void getHelloWorld() {
+        assert SampleUtils.getHelloWorld().equals("Hello World");
+    }
+}
