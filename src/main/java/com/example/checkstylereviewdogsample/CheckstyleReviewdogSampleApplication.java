21634a9a16f7ec4f5605a8d85ae21b1be122e597
y.nakamura <y.nakamura@linecorp.com>
2023-12-12 08:07:23 +0900

    Initialize Spring Boot

---
+++ src/main/java/com/example/checkstylereviewdogsample/CheckstyleReviewdogSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkstylereviewdogsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstyleReviewdogSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckstyleReviewdogSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/checkstylereviewdogsample/CheckstyleReviewdogSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkstylereviewdogsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleReviewdogSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
